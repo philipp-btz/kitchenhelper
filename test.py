bcf9a2bed12df14d01059479bb32a797da450697
Philipp <162928651+philipp-btz@users.noreply.github.com>
2026-03-07 15:36:37 +0100

    status testing

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
+import time
 import kitchenhelper as kh
 from escpos.printer import Network
 
-printer = Network("192.168.8.188", port=9100, profile="ITPP047")
+printer = Network("192.168.8.188", port=9100, profile="TM-T88V")
+
+while True:
+    print(f"Printer is Usable {printer.is_usable()}")
+    print(f"Printer is Online {printer.is_online()}")
+    print(f"Paper Status {printer.paper_status()}")
+    print(printer._raw(b"\x10\x04\x01"))
+    time.sleep(0.5)
 
 

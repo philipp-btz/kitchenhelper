4e8e3edb9e9c7aacfea848f16b74bdad48581387
Philipp <162928651+philipp-btz@users.noreply.github.com>
2026-03-14 19:30:27 +0100

    added .env

--- test.py
+++ test.py
@@ -1,14 +1,7 @@
-import time
-import kitchenhelper as kh
-from escpos.printer import Network
-
-printer = Network("192.168.8.188", port=9100, profile="TM-T88V")
-
-while True:
-    print(f"Printer is Usable {printer.is_usable()}")
-    print(f"Printer is Online {printer.is_online()}")
-    print(f"Paper Status {printer.paper_status()}")
-    print(printer._raw(b"\x10\x04\x01"))
-    time.sleep(0.5)
+import dotenv
+import os
 
 
+dotenv.load_dotenv("static_condddddfig.env")
+print(os.environ)
+print("hi")

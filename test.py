b3ca1b53d044bdd36cd26a04c74c3d075021a5b9
Philipp <162928651+philipp-btz@users.noreply.github.com>
2026-03-16 17:55:07 +0100

    housekeeping

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-import dotenv
+from pathlib import Path
 import os
-
-
-#os.environ["KITCHENHELPER_DB_PATH"] = ".local/orders.db"
-if os.environ.get("KITCHENHELPER_DB_PATH") is not None:
-    print(os.environ.get("KITCHENHELPER_DB_PATH"))
-print(os.environ)
-print("hi")
+import json
+'''
+output_file = Path("some/path/file.txt")
+output_file.parent.mkdir(exist_ok=True, parents=True)
+output_file.write_text("some text")
+'''
+defaults = {}
+if os.path.isfile(".local/user_settings/user_settings.json"):
+    with open(".local/user_settings/user_settings.json", "r", encoding="utf-8") as f:
+        settings_dict = json.load(f)
+        defaults.update(settings_dict)
+        for key, value in settings_dict.items():
+            os.environ[key] = value
+            print("addasd")
+print("ok")

ccd6c3fba17e59e71199dec73cbcc484ca08bff9
Philipp <162928651+philipp-btz@users.noreply.github.com.>
2026-05-14 10:14:13 +0200

    chore: remove old Flask/gunicorn files replaced by cleanroom remake

--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-# Expose the Flask `app` as the WSGI callable named `application`
-# so servers like Gunicorn or Waitress can import it as `wsgi:application`.
-application = app
